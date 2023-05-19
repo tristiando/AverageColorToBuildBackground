1ddd62ab2a6d6cee24127605e23a261e6a447e90
Thu Do <dothu.khtn@gmail.com>
2023-05-19 09:35:57 +0700

    Initial Commit

---
+++ AverageColorToBuildBackground/AverageColorToBuildBackgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AverageColorToBuildBackgroundApp.swift
+//  AverageColorToBuildBackground
+//
+//  Created by Thu Do on 19/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AverageColorToBuildBackgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AverageColorToBuildBackground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AverageColorToBuildBackground
+//
+//  Created by Thu Do on 19/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
